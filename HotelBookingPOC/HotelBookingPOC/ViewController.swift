a84e451d08d8055f2c88af4bc7e2d11378e4e106
aungkhanthtoo <aungkhanthtoo1@gmail.com>
2019-09-04 15:58:14 +0630

    commit.

---
+++ HotelBookingPOC/HotelBookingPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelBookingPOC
+//
+//  Created by Htoo Aung Kyaw on 9/4/19.
+//  Copyright © 2019 Htoo Aung Kyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
